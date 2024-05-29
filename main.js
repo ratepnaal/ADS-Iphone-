a262ba56c0c61de9b1365b4f7d185f029a0bc7bf
ratepnaal <ratepn445@gmail.com>
2024-05-29 21:02:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.imag');
+let containar = document.querySelector('.containar')
+function phones(phone){
+    img.src=phone;
+}
+function colors(color){
+    containar.style.background = color;
+}
